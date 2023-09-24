7d17fdd28cdca2faf50550a6f7d223aad5d2cfa3
Crowdbotics <team@crowdbotics.com>
2023-09-24 08:19:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quiet-heart-43535",
+  url: "https://quiet-heart-43535.botics.co",
+  api: "https://quiet-heart-43535.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
